bb692694bd9382dcfcb3ac0b6de029032f24c2a9
Ali Boujdaria <86597426+aliboujdaria@users.noreply.github.com>
2024-04-16 16:01:41 +0200

    Remove outdated keys

--- ovh/cloud-infra-wordpress/provider.tf
+++ ovh/cloud-infra-wordpress/provider.tf
@@ -11,7 +11,7 @@ provider "template" {}
 provider "ovh" {
   alias              = "ovh"
   endpoint           = "ovh-eu"
-  application_key    = "1961f136c5b03711"
-  application_secret = "b4417ce43ff41c70c39f51d5edeeb068"
-  consumer_key       = "0654bbc1465bee0929f34a5877d9608c"
+  application_key    = ""
+  application_secret = ""
+  consumer_key       = ""
 }
